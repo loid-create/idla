9e91a6d395a33d06a1a22a2ddf78010b9b17549b
loid-create <idla3007@gmail.com>
2020-08-12 15:01:04 +0700

    changes

--- application/views/dokter/konsultasi-online.php
+++ application/views/dokter/konsultasi-online.php
@@ -4,7 +4,6 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">Konsultasi Online</h1>
 
-<<<<<<< HEAD
     <section class="content">
         <div class="row">
             <div class="col-md-8" id="chatSection">
@@ -101,64 +100,6 @@
                 <!--/.box -->
             </div>
             <!-- /.col -->
-=======
-    <div class="card bg-light mb-3">
-        <div class="card-body text-center py-5">
-            <h5 class="card-title">Antrian Masih Kosong</h5>
-            <p class="card-text"><small class="text-muted">Semua antrian akan ditampilkan disini</small></p>
-        </div>
-    </div>
-
-    <div class="card bg-light mb-3 shadow-sm"">
-        <div class="card-body">
-            <div class="row border-bottom pb-4">
-                <div class="col mr-4" style="max-width:80px;">
-                    <img class="rounded-circle" src="https://fakeimg.pl/80x80/">
-                </div>
-                <div class="col">
-                    <p class="card-text mb-0 font-weight-bold">Agung Handoko</p>
-                    <p class="card-text mb-1"><small class="text-muted">Kucing</small></p>
-                    <p class="card-text">Tidak nafsu makan dan muntah-muntah</p>
-                </div>
-                <div class="col text-right">
-                    <p class="card-text font-weight-bold">12/8</p>
-                </div>
-            </div>
-            <div class="row mt-2">
-                <div class="col align-self-center">
-                    <p class="font-italic">Percakapan ini telah ditutup</p>
-                </div>
-                <div class="col text-right align-self-center">
-                    <button class="btn btn-info">Chat Sekarang</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="card bg-light mb-3 shadow-sm">
-        <div class="card-body">
-            <div class="row border-bottom pb-4">
-                <div class="col mr-4" style="max-width:80px;">
-                    <img class="rounded-circle" src="https://fakeimg.pl/80x80/">
-                </div>
-                <div class="col">
-                    <p class="card-text mb-0 font-weight-bold">Agung Handoko</p>
-                    <p class="card-text mb-1"><small class="text-muted">Kucing</small></p>
-                    <p class="card-text">Tidak nafsu makan dan muntah-muntah</p>
-                </div>
-                <div class="col text-right">
-                    <p class="card-text font-weight-bold">12/8</p>
-                </div>
-            </div>
-            <div class="row mt-2">
-                <div class="col align-self-center">
-                    <p class="font-italic">Percakapan ini telah ditutup</p>
-                </div>
-                <div class="col text-right align-self-center">
-                    <button class="btn btn-info">Chat Sekarang</button>
-                </div>
-            </div>
->>>>>>> 08aed4fbc1ffa433375bff77ec4c3192c9929194
         </div>
         <!-- /.row -->
     </section>
--- application/views/dokter/medical-record.php
+++ application/views/dokter/medical-record.php
@@ -3,17 +3,9 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">Medical Record</h1>
-<<<<<<< HEAD
     <div class="card bg-light mb-3" style="max-width: 100%;">
         <div class="card-header"><strong>Riwayat Data Medis</strong></div>
         <div class="card-body">
-=======
-
-    <div class="card bg-light mb-3" style="max-width: 100%;">
-        <div class="card-header"><strong>Riwayat Data Medis</strong></div>
-        <div class="card-body">
-
->>>>>>> 08aed4fbc1ffa433375bff77ec4c3192c9929194
             <table id="dataMedis" class="table table-striped table-bordered" style="width:100%">
                 <thead>
                     <tr>
@@ -27,7 +19,6 @@
                     </tr>
                 </thead>
                 <tbody>
-<<<<<<< HEAD
                     <tr>
                         <td>1</td>
                         <td>Sapi - Domestic (Spaying)</td>
@@ -67,49 +58,6 @@
 
                 </tbody>
             </table>
-=======
-
-                        <tr>
-                            <td>1</td>
-                            <td>Sapi - Domestic (Spaying)</td>
-                            <td>Agung Handoko</td>
-                            <td>Kucing</td>
-                            <td>drh.Ari Ramadhan</td>
-                            <td>12-08-2020</td>
-                            <td>Terkena gejala kutu bla bla bla bla</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Sapi - Domestic (Spaying)</td>
-                            <td>Agung Handoko</td>
-                            <td>Kucing</td>
-                            <td>drh.Ari Ramadhan</td>
-                            <td>12-08-2020</td>
-                            <td>Terkena gejala kutu bla bla bla bla</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Sapi - Domestic (Spaying)</td>
-                            <td>Agung Handoko</td>
-                            <td>Kucing</td>
-                            <td>drh.Ari Ramadhan</td>
-                            <td>12-08-2020</td>
-                            <td>Terkena gejala kutu bla bla bla bla</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Sapi - Domestic (Spaying)</td>
-                            <td>Agung Handoko</td>
-                            <td>Kucing</td>
-                            <td>drh.Ari Ramadhan</td>
-                            <td>12-08-2020</td>
-                            <td>Terkena gejala kutu bla bla bla bla</td>
-                        </tr>
- 
-                </tbody>
-            </table>
-
->>>>>>> 08aed4fbc1ffa433375bff77ec4c3192c9929194
         </div>
     </div>
 </div>
